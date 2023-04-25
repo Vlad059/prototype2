addded5f723b8d5bfa483d58c49c0ceb64b8e415
Vlad059 <v.matveev@maestro3d.pro>
2023-04-25 16:39:42 +0500

    В App виджете использованы Providers

--- lib/features/app/app.dart
+++ lib/features/app/app.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class App extends StatelessWidget {
+import './di/app_scope.dart';
+
+import '../common/service/style/style_service.dart';
+
+class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  State<App> createState() => _AppState();
+}
 
+class _AppState extends State<App> {
+  late final IStyleService _appStyles;
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider<IAppScope>(
+      create: (_){
+        final appScope = AppScope();
+
+        _appStyles = appScope.appStyles;
+
+        return appScope;
+      },
+      child: ChangeNotifierProvider<IStyleService>(
+        create: (_) => _appStyles,
+        child: Consumer<IStyleService>(
+          builder: (_,__,___) {
+            return const MaterialApp(
+
+            );
+          },
+        ),
+      ),
     );
   }
 }
+
--- lib/features/app/di/app_scope.dart
+++ lib/features/app/di/app_scope.dart
@@ -1,6 +1,11 @@
 import '../../common/service/style/style_service.dart';
 import '../../common/service/style/style_service_impl.dart';
 
-class AppScope {
+abstract class IAppScope {
+  IStyleService get appStyles;
+}
+
+class AppScope extends IAppScope{
+  @override
   IStyleService appStyles = StyleService();
 }
